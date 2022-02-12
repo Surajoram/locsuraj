181d9c331fa57c0ee88b74214ca43527bff73f86
Surajoram <99544377+Surajoram@users.noreply.github.com>
2022-02-12 20:53:13 +0530

    Add files via upload

---
+++ evenodd.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int num;
+    printf("enter your number:");
+    scanf("%d", &num);
+    
+    if(num%2==0)
+    {
+    printf("%d is even.", num);
+    }
+    else
+    {
+    printf("%d is odd.", num);
+    }
+    
+    return 0;
+}
